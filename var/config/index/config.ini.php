0b5c6808321a0f44260661e03107be6f0aa0adf1
Ornythorink <orny2007@gmail.com>
2014-02-19 21:12:19 +0100

    reintegration du dossier var

---
+++ var/config/index/config.ini.php
@@ -0,0 +1,9 @@
+;<?php die(''); ?>
+;for security reasons , don't remove or modify the first line
+
+startModule=vroum
+startAction="default:index"
+
+[responses]
+[modules]
+vroum.access=2
---
+++ var/config/installer.ini.php
@@ -0,0 +1,34 @@
+;<?php die(''); ?>
+; for security reasons , don't remove or modify the first line
+; don't modify this file if you don't know what you do. it is generated automatically by jInstaller
+
+
+[index]
+jelix.installed=1
+jelix.version=1.5.3
+jacl.installed=0
+jacl.version=
+jacl2.installed=0
+jacl2.version=
+jacl2db.installed=0
+jacl2db.version=
+jacldb.installed=0
+jacldb.version=
+jauth.installed=0
+jauth.version=
+jauthdb.installed=0
+jauthdb.version=
+jpref.installed=0
+jpref.version=
+jsoap.installed=0
+jsoap.version=
+junittests.installed=0
+junittests.version=
+jelix.version.date="2013-09-19 10:56"
+jelix.firstversion=1.5.3
+jelix.firstversion.date="2013-09-19 10:56"
+vroum.installed=1
+vroum.version=0.1pre
+[__modules_data]
+jelix.contexts="db:default"
+
